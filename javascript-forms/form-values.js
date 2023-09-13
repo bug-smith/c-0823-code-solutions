528228526477e74b49693e8550632c215b2a6304
Bug Smith <salttyyy@gmail.com>
2023-09-13 17:31:44 +0000

    revised form-values

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -2,6 +2,10 @@ const $form = document.querySelector('#contact-form');
 
 $form.addEventListener('submit', function (event) {
   event.preventDefault();
-  const obj = $form.name.value + $form.email.value + $form.message.value;
+  const obj = {
+    name: $form.name.value,
+    email: $form.email.value,
+    message: $form.message.value,
+  };
   console.log(obj);
 });
