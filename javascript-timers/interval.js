cf3f51311706d420fb2357d9e3f1a62d322c08ac
Bug Smith <salttyyy@gmail.com>
2023-09-25 19:13:17 +0000

    completed clearInterval

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -2,13 +2,14 @@ const $h1 = document.querySelector('h1');
 
 let countdown = 4;
 
+const intervalID = setInterval(countDown, 1000);
+
 function countDown() {
   if (countdown > 0) {
     countdown--;
     $h1.textContent = countdown;
   } else {
     $h1.textContent = '~Earth Beeeelooowww Us~';
+    clearInterval(intervalID);
   }
 }
-
-setInterval(countDown, 1000);
