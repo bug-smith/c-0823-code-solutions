fd5e7c043656e9b5ff0e82fa6929217154548041
Bug Smith <salttyyy@gmail.com>
2023-10-13 15:59:22 +0000

    completed state

---
+++ react-state/my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import ToggleButton from './Components/ToggleButton';
+
+function App() {
+  return (
+    <>
+      <ToggleButton text="button 1" color="red" />
+      <ToggleButton text="button 2" color="green" />
+      <ToggleButton text="button 3" color="blue" />
+    </>
+  );
+}
+
+export default App;
