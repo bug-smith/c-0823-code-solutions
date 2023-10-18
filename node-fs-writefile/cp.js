ac9ba62b9abcc204d61e140f0f7f9ddce9eea0ef
Bug Smith <salttyyy@gmail.com>
2023-10-18 22:45:12 +0000

    completed

--- node-fs-writefile/cp.js
+++ node-fs-writefile/cp.js
@@ -1,10 +1,9 @@
 import { readFile, writeFile } from 'node:fs/promises';
 
+const [, , source, dest] = process.argv;
 try {
-  const promise = process.argv[2];
-  const reading = await readFile(promise, 'utf8');
-  console.log(reading);
-  await writeFile('');
+  const reading = await readFile(source);
+  await writeFile(dest, reading);
 } catch (e) {
   console.error(e);
 }
