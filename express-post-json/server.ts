ffb3ac04acf28a40b3c1e6d09bb15498471c6be5
Bug Smith <salttyyy@gmail.com>
2023-10-20 21:08:39 +0000

    completed revisions, updated error catching, aswell as type declaration

--- express-post-json/server.ts
+++ express-post-json/server.ts
@@ -7,34 +7,32 @@ type Grade = {
   score: number;
 };
 
-type NextId = number;
-
 const grades: Record<number, Grade> = {};
 
-let nextId: NextId = 1;
+let nextId = 1;
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/api/grades', (req, res) => {
   const localArr: Grade[] = Object.values(grades);
   res.json(localArr);
 });
 
-app.use(express.json());
-
 app.post('/api/grades', (req, res) => {
   const grade = req.body;
-  grade.id = nextId;
-  grades[nextId] = grade;
-  nextId++;
-
   if (
     typeof !grade.name === 'string' ||
     typeof !grade.course === 'string' ||
     typeof !grade.score === 'number'
   ) {
-    res.status(204);
+    res.sendStatus(400);
+    return;
   }
+  grade.id = nextId;
+  grades[nextId] = grade;
+  nextId++;
   res.json(grade);
   res.send();
 });
