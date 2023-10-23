526e9389dfa079adeddd9386273eb5afbf314586
Bug Smith <salttyyy@gmail.com>
2023-10-23 23:37:03 +0000

    completed solution

--- express-notes-api/server.ts
+++ express-notes-api/server.ts
@@ -1,97 +1,134 @@
-import express from 'express'
-import { readFile, writeFile} from 'node:fs/promises'
-
+/* eslint-disable @typescript-eslint/no-unused-vars -- REMOVE ME */
+import { readFile, writeFile } from 'node:fs/promises';
+import express from 'express';
 
 type Note = {
-  id: number,
-  content: string,
-}
-
+  id: number;
+  content: string;
+};
 type Data = {
-  nextId: number,
-  notes: Record<number, Note>
-}
-const notes: { [key: number]: Note } = {};
-const dataNextId = 1
+  nextId: number;
+  notes: Record<string, Note>;
+};
 
-async function readData() : Promise<Data> {
-  const data = await readFile('data.json', 'utf8')
-  return JSON.parse(data)
-}
+const app = express();
+app.use(express.json());
 
-async function writeData (data: Data): Promise<void> {
-  const value = JSON.stringify(data, null, 2)
-  await writeFile('data.json', value, 'utf8')
+async function readData(): Promise<Data> {
+  const data = await readFile('./data.json');
+  return JSON.parse(data.toString());
 }
 
-const app = express()
-
-
-app.use(express.json())
-
-app.get('/api/notes/', async (req, res) => {
-const data = await readData()
-const notes: Note[] = []
-for (const id in data.notes){
-  notes.push(data.notes[id])
+async function writeData(data: Data) {
+  await writeFile('./data.json', JSON.stringify(data, null, 2));
 }
-res.json(data)
-})
 
-
-app.get('/api/notes/:id', async (req, res) => {
-  const data = await readData()
-  const { id } = req.params
-  res.json(data.notes[+id])
+app.get('/api/notes', async (req, res) => {
+  try {
+    const data = await readData();
+    const notes: Note[] = [];
+    for (const id in data.notes) {
+      notes.push(data.notes[id]);
+    }
+    res.json(notes);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'an unexpected error occurred' });
+  }
 });
 
-
-app.post('/api/notes', async (req, res)=>{
+app.get('/api/notes/:id', async (req, res) => {
+  try {
     const data = await readData();
-    const { content } = req.body
-    const newNote = {
+    const id = Number(req.params.id);
+    if (Number.isNaN(id) || !Number.isInteger(id) || id < 1) {
+      res.status(400).json({ error: 'id must be a positive integer' });
+      return;
+    }
+    if (data.notes[id] === undefined) {
+      res.status(404).json({ error: `cannot find note with id ${id}` });
+      return;
+    }
+    res.json(data.notes[id]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'an unexpected error occurred' });
+  }
+});
+
+app.post('/api/notes', async (req, res) => {
+  try {
+    const { content } = req.body;
+    if (content === undefined) {
+      res.status(400).json({ error: 'content is a required field' });
+      return;
+    }
+    const data = await readData();
+    const note = {
       id: data.nextId,
       content,
-    }
-    data.notes[data.nextId++] = newNote
-    await writeData(data)
-    res.status(201).json()
-})
-
-app.put('/api/notes/:id', async (req, res) =>{
-    const data = await readData();
-    const { id } = req.params;
-    const { content } = req.body
-    const note = {
-      id: +id,
-      content,
-    }
-    data.notes[+id] = note
-    await writeData(data)
-    res.json(note)
-}
-)
+    };
+    data.notes[note.id] = note;
+    data.nextId++;
+    await writeData(data);
+    res.status(201).json(note);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'an unexpected error occurred' });
+  }
+});
 
 app.delete('/api/notes/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    if (Number.isNaN(id) || !Number.isInteger(id) || id < 1) {
+      res.status(400).json({ error: 'id must be a positive integer' });
+      return;
+    }
     const data = await readData();
-    const { id } = req.params;
-    delete data.notes[+id]
-    await writeData(data)
-    res.sendStatus(400)
-})
+    if (data.notes[id] === undefined) {
+      res.status(404).json({ error: `cannot find note with id ${id}` });
+      return;
+    }
+    delete data.notes[id];
+    await writeData(data);
+    res.sendStatus(204);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'an unexpected error occurred' });
+  }
+});
 
+app.put('/api/notes/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    if (Number.isNaN(id) || !Number.isInteger(id) || id < 1) {
+      res.status(400).json({ error: 'id must be a positive integer' });
+      return;
+    }
+    const { content } = req.body;
+    if (content === undefined) {
+      res.status(400).json({ error: 'content is a required field' });
+      return;
+    }
+    const data = await readData();
+    if (data.notes[id] === undefined) {
+      res.status(404).json({ error: `cannot find note with id ${id}` });
+      return;
+    }
+    const note = {
+      id,
+      content,
+    };
+    data.notes[note.id] = note;
+    await writeData(data);
+    res.json(note);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'an unexpected error occurred' });
+  }
+});
 
-
-app.listen('8080', () => {
-  console.log('server is listening')
-})
-
-readData()
-
-
-// first thing is layout the routes, think about how many is needed.
-
-// import read and write, decalre express obj
-
-// routes:
-// to get all is
+app.listen(8080, () => {
+  console.log(`express server listening on port 8080`);
+});
