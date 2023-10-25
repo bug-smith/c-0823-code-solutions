97935876d8b0fe435a7c0a82c6e4226943378607
Bug Smith <salttyyy@gmail.com>
2023-10-25 18:47:08 +0000

    completed country

--- sql-join/where-are-they.sql
+++ sql-join/where-are-they.sql
@@ -1,5 +1,7 @@
 select "addresses"."line1" as "Address Line",
 "cities"."cityId" as "City Name",
 "addresses"."district" as "Address District"
+"countries"."name" as "Country Name"
 from "addresses"
 join "cities" using ("cityId");
+join "countries" using ("countryId")
