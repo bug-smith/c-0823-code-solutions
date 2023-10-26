2e39052da9362d25300c80a3585b0c8091907149
Bug Smith <salttyyy@gmail.com>
2023-10-26 21:23:35 +0000

    completed

---
+++ react-hot-button-component/hot-button-app/src/App.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState('');
+  useEffect(() => {
+    changeStyle();
+  });
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+  const changeStyle = () => {
+    if (count === 3) {
+      setColor('#351C75');
+    } else if (count === 6) {
+      setColor('#674EA7');
+    } else if (count === 9) {
+      setColor('#E06666');
+    } else if (count === 12) {
+      setColor('#F6B26B');
+    } else if (count === 15) {
+      setColor('#FFFA05');
+    } else if (count === 18) {
+      setColor('#FFFFFF');
+    }
+  };
+  return (
+    <>
+      <button
+        onClick={incrementCount}
+        style={{ background: color, color: 'grey' }}>
+        Hot Button
+      </button>
+      <button
+        onClick={incrementCount}
+        style={{ background: 'yellow', color: 'black' }}>
+        {count}
+      </button>
+    </>
+  );
+}
+
+export default App;
