6dc82964f230349f9b2167f80cea9efb9589d4a7
Bug Smith <salttyyy@gmail.com>
2023-10-27 20:35:15 +0000

    completeD

---
+++ react-rendering-lists/render-list-app/src/PokemonList.tsx
@@ -0,0 +1,12 @@
+import { Pokemon } from './App';
+
+type Props = {
+  pokedex: Pokemon[];
+};
+
+export function PokemonList({ pokedex }: Props) {
+  const listItems = pokedex.map((pokemon) => (
+    <li key={pokemon.number}> {pokemon.name}</li>
+  ));
+  return <ul>{listItems}</ul>;
+}
