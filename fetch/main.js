de9e5924d289813a2c62eccb51cdcdd1f1d8e925
Bug Smith <salttyyy@gmail.com>
2023-11-06 18:02:28 +0000

    completed

--- fetch/main.js
+++ fetch/main.js
@@ -1,17 +1,20 @@
 async function getData() {
-  try {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+if (!response.ok){
+  new Error(`The reponse was unsucessful with the status Code - ${reponse.status}`)
+}
+  try {
     const data = await response.json();
     console.log(data);
   } catch (e) {
-    console.error(e);
+    console.error(Error);
   }
 }
 
 async function pokeData() {
   const response = await fetch('https://pokeapi.co/api/v2/pokemon/1')
-  if (response.ok === 'false'){
-    new Error('the reponse has failed error 404');
+  if (!response.ok){
+    new Error(`The reponse was unsucessful with the status Code - ${reponse.status}`);
   }
   try {
     const pokeData = await response.json();
