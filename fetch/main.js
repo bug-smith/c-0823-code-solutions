cf7f47cf2cd0f052c1c6b781dc153f11fc71c65e
Bug Smith <salttyyy@gmail.com>
2023-11-06 18:18:55 +0000

    completed revisions of try + error handling

--- fetch/main.js
+++ fetch/main.js
@@ -1,27 +1,31 @@
 async function getData() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-if (!response.ok){
-  new Error(`The reponse was unsucessful with the status Code - ${reponse.status}`)
-}
-  try {
-    const data = await response.json();
-    console.log(data);
-  } catch (e) {
-    console.error(Error);
-  }
+    try {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      if (!response.ok) {
+        new Error(
+          `The reponse was unsucessful with the status Code - ${reponse.status}`
+        );
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
 }
 
 async function pokeData() {
+    try {
   const response = await fetch('https://pokeapi.co/api/v2/pokemon/1')
   if (!response.ok){
     new Error(`The reponse was unsucessful with the status Code - ${reponse.status}`);
   }
-  try {
     const pokeData = await response.json();
     console.log(pokeData);
     console.log(response);
   } catch (e) {
-    console.error(Error);
+    console.error(e);
   }
 }
 
