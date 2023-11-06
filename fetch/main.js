23b61020d94e6370ffa1803c1981148f28fbbbb0
Bug Smith <salttyyy@gmail.com>
2023-11-06 18:29:42 +0000

    completed

--- fetch/main.js
+++ fetch/main.js
@@ -1,26 +1,26 @@
 async function getData() {
-    try {
-      const response = await fetch(
-        'https://jsonplaceholder.typicode.com/users'
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!response.ok) {
+      throw new Error(
+        `The reponse was unsucessful with the status Code - ${response.status}`
       );
-      if (!response.ok) {
-        new Error(
-          `The reponse was unsucessful with the status Code - ${reponse.status}`
-        );
-      }
-      const data = await response.json();
-      console.log(data);
-    } catch (e) {
-      console.error(e);
     }
+    const data = await response.json();
+    console.log(data);
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 async function pokeData() {
-    try {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon/1')
-  if (!response.ok){
-    new Error(`The reponse was unsucessful with the status Code - ${reponse.status}`);
-  }
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
+    if (!response.ok) {
+      throw new Error(
+        `The reponse was unsucessful with the status Code - ${response.status}`
+      );
+    }
     const pokeData = await response.json();
     console.log(pokeData);
     console.log(response);
